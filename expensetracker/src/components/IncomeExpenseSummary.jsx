dabb309db25b468fbe2d6437964bb0b57fa8b5eb
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-04-04 10:28:08 -0400

    updated

--- expensetracker/src/components/IncomeExpenseSummary.jsx
+++ expensetracker/src/components/IncomeExpenseSummary.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import '../styles/IncomeExpenseSummary.css';
 const IncomeExpenseSummary = () => {
   return (
-    <div>
-      IncomeExpenseSummary
+    <div className='IncomeExpenseSummary'> 
+      <hr/>
+      <div className='ExpenseDiv'>
+        <div className='BalanceDiv'>
+          <p >INCOME</p>
+          <p>$100.00</p>
+        </div>
+        <div className='BalanceDiv'>
+          <p >EXPENSE</p>
+          <p>$50.00</p>
+        </div>
+     </div>
     </div>
   )
 }
--- expensetracker/src/components/TotalExpenseDisplay.jsx
+++ expensetracker/src/components/TotalExpenseDisplay.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import '../styles/TotalExpenseDisplay.css';
 const TotalExpenseDisplay = () => {
   return (
-    <div>
-      TotalExpenseDisplay
+    <div className='TotalExpenseDisplay'>
+      <div className='YourBalance'>YOUR BALANCE</div>
+      <div className='Balance'>$10000.00</div>
     </div>
   )
 }
